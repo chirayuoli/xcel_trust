67ab6fd318a729e8d971c1876029acd37f3c0936
chirayuoli <chirayuoli56@gmail.com>
2023-06-24 09:31:16 +0545

    UI for displaying mnemonics and addresses

--- App.tsx
+++ App.tsx
@@ -1,117 +1,128 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  ActivityIndicator,
+  NativeModules,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const {RNTrustWallet} = NativeModules;
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [walletData, setWalletData] = useState({
+    mnemonic: '',
+    addresses: {
+      ETH: '',
+      BTC: '',
+      BNB: '',
+    },
+  });
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    generateMnemonic();
+  }, []);
+
+  const generateMnemonic = async () => {
+    try {
+      const [
+        mnemonicResult,
+        addressETHResult,
+        addressBTCResult,
+        addressBNBResult,
+      ] = await RNTrustWallet.generateMnemonic();
+
+      const updatedWalletData = {
+        mnemonic: mnemonicResult,
+        addresses: {
+          ETH: addressETHResult,
+          BTC: addressBTCResult,
+          BNB: addressBNBResult,
+        },
+      };
+
+      setWalletData(updatedWalletData);
+      setDataLoaded(true);
+    } catch (error) {
+      console.error('Error generating mnemonic:', error);
+    }
+  };
+
+  const handleRegenerateMnemonic = () => {
+    setWalletData({
+      mnemonic: '',
+      addresses: {
+        ETH: '',
+        BTC: '',
+        BNB: '',
+      },
+    });
+    setDataLoaded(false);
+    generateMnemonic();
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      {dataLoaded ? (
+        <>
+          <Text style={styles.text}>Mnemonic: {walletData.mnemonic}</Text>
+          <View style={styles.addressList}>
+            {Object.entries(walletData.addresses).map(([currency, address]) => (
+              <View style={styles.addressItem} key={currency}>
+                <Text style={styles.name}>{currency}:</Text>
+                <Text style={styles.address}>{address}</Text>
+              </View>
+            ))}
+          </View>
+          <Button
+            title="Regenerate Mnemonic"
+            onPress={handleRegenerateMnemonic}
+          />
+        </>
+      ) : (
+        <ActivityIndicator size="large" />
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  text: {
+    fontSize: 16,
+    marginVertical: 8,
+    color: 'white',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  addressList: {
+    marginVertical: 16,
+    backgroundColor: '#555555',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 8,
+    width: '90%',
   },
-  highlight: {
-    fontWeight: '700',
+  addressItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  name: {
+    width: 60,
+    marginRight: 8,
+    color: 'white',
+  },
+  address: {
+    flex: 1,
+    flexWrap: 'wrap',
+    color: 'white',
   },
 });
 
